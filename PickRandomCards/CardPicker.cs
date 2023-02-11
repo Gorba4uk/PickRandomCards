dec47f2313bae68d817964a22a63dc21dd037ee1
Bohdan Horbachuk <gorba4uk1991@gmail.com>
2023-02-11 14:46:39 +0100

    Dodaj pliki projektów.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for(int i =0; i< numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        public static string RandomSuit()
+        {
+            int value = random.Next(1,5);
+            if (value == 1) return "Pik";
+            else if (value == 2) return "Kier";
+            else if (value == 3) return "Trefl";
+            else return "Karo";           
+        }
+        public static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "As";
+            else if (value == 11) return "Walet";
+            else if (value == 12) return "Dama";
+            else if (value == 13) return "Krol";
+            else return value.ToString();
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            run();
+        }
+
+        static void run()
+        {
+            string[] cards = CardPicker.PickSomeCards(5);
+            for(int i =0; i<cards.Length; i++)
+            {
+                Console.WriteLine(cards[i]);
+            }
+        }
+    }
+}
